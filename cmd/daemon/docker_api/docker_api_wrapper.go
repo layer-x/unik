beb431d7caced8c9d449b791894103f9ecd544cf
Scott Weiss <scott.weiss@emc.com>
2016-02-03 00:43:53 -0500

    run container
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- cmd/daemon/docker_api/docker_api_wrapper.go
+++ cmd/daemon/docker_api/docker_api_wrapper.go
@@ -7,6 +7,8 @@ import (
 	"github.com/layer-x/layerx-commons/lxmartini"
 	"github.com/layer-x/layerx-commons/lxerrors"
 	"github.com/layer-x/unik/cmd/daemon/main/ec2api"
+"encoding/json"
+"io/ioutil"
 )
 
 
@@ -40,6 +42,37 @@ func AddDockerApi(m *martini.ClassicMartini) *martini.ClassicMartini {
 		}
 		lxlog.Debugf(logrus.Fields{"dockerUnikernels": dockerUnikernels}, "Listing all unikernels for docker")
 		lxmartini.Respond(res, dockerUnikernels)
+	})
+	m.Post("/v1.20/containers/create", func(res http.ResponseWriter, req *http.Request) {
+		defer req.Body.Close()
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"req": req, "err": err}, "could not read request body")
+			lxmartini.Respond(res, lxerrors.New("could not read request body", err))
+			return
+		}
+		var runRequest DockerRunRequest
+		err = json.Unmarshal(body, &runRequest)
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"body": string(body), "err": err}, "could not unmarshal body to docker run request json")
+			lxmartini.Respond(res, lxerrors.New("could not unmarshal body to docker run request json", err))
+			return
+		}
+		instanceName := runRequest.Hostname
+		unikernelName := runRequest.Image
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"err":err, "instancess": 1, "unikernel_name": unikernelName}, "invalid input for field 'instances'")
+			lxmartini.Respond(res, err)
+			return
+		}
+		instanceIds, err := ec2api.RunApp(unikernelName, instanceName, 1)
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"err":err, "unikernel_name": unikernelName}, "launching 1 instances of unikernel "+unikernelName+" for docker")
+			lxmartini.Respond(res, err)
+			return
+		}
+		lxlog.Infof(logrus.Fields{"instance_ids": instanceIds, "request": runRequest},"1 instances started of unikernel "+unikernelName+" for docker")
+		lxmartini.Respond(res, DockerRunResponse{Id: instanceIds[0]})
 	})
 	return m
 }
