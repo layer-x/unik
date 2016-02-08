66b727474690c37723659663a396a9cad84be583
Scott Weiss <scott.weiss@emc.com>
2016-02-08 16:38:42 -0500

    add logs, delete unikernel, list uks
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- unik_client/unik_client.go
+++ unik_client/unik_client.go
@@ -7,6 +7,8 @@ import (
 "net/http"
 "fmt"
 	"strings"
+"io"
+	"bufio"
 )
 
 type UnikClient struct {
@@ -59,4 +61,54 @@ func (c *UnikClient) RunUnikernel(unikernelName, instanceName string, instances 
 		return lxerrors.New("failed running unikernel, got message: " + string(body), err)
 	}
 	return nil
+}
+
+func (c *UnikClient) ListUnikernels() ([]*types.Unikernel, error) {
+	_, body, err := lxhttpclient.Get(c.url, fmt.Sprintf("/unikernels?verbose=%v", false), nil)
+	if err != nil {
+		return nil, lxerrors.New("failed retrieving unikernels", err)
+	}
+	var unikernels []*types.Unikernel
+	err = json.Unmarshal(body, &unikernels)
+	if err != nil {
+		return nil, lxerrors.New("failed to retrieve unikernels: "+string(body), err)
+	}
+	return unikernels, nil
+}
+
+func (c *UnikClient) DeleteUnikernel(unikernelName string, force bool) error {
+	resp, body, err := lxhttpclient.Delete(c.url, "/unikernels/"+unikernelName+fmt.Sprintf("?force=%v", force), nil)
+	if err != nil {
+		return lxerrors.New("failed deleting unikernel", err)
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return lxerrors.New("failed deleting unikernel, got message: "+string(body), err)
+	}
+	return nil
+}
+
+func (c *UnikClient) GetUnikInstanceLogs(unikInstanceId string) (string, error) {
+	resp, body, err := lxhttpclient.Get(c.url, "/instances/"+unikInstanceId+"/logs"+fmt.Sprintf("?follow=%v", false), nil)
+	if err != nil {
+		return "", lxerrors.New("failed retrieving logs", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", lxerrors.New("failed retrieving logs, got message: "+string(body), err)
+	}
+	return string(body), nil
+}
+
+func (c *UnikClient) FollowUnikInstanceLogs(unikInstanceId string, stdout io.Writer) error {
+	resp, err := http.Get("http://" + c.url + "/instances/" + unikInstanceId + "/logs" + fmt.Sprintf("?follow=%v", true))
+	if err != nil {
+		return lxerrors.New("error performing GET request", err)
+	}
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			return lxerrors.New("reading line", err)
+		}
+		stdout.Write(append(line, byte('\n')))
+	}
 }
