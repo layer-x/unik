dd5822aecffac408b0cae487f186c8eb943df850
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-03-08 10:59:32 +0200

    applied ppb patch

--- containers/rumpcompiler/base/fixrump.sh
+++ containers/rumpcompiler/base/fixrump.sh
@@ -10,5 +10,19 @@ if [ "$PLATFORM" = "" ]; then
   exit 1
 fi
 
+
+if [ "$PLATFORM" = "hw" ]; then
+# ppb patch
+cat >>  /opt/rumprun/src-netbsd/sys/rump/dev/lib/libpci/PCI.ioconf <<EOF
+
+    pci*    at ppb? bus ?
+    ppb*    at pci? dev ? function ?
+EOF
+
+sed -i -e 's/SRCS+=	pci.c/SRCS+=	ppb.c ci.c' /opt/rumprun/src-netbsd/sys/rump/dev/lib/libpci/Makefile
+
+
 ./build-rr.sh -d $DESTDIR -o ./obj $PLATFORM build -- $BUILDRUMP_EXTRA && \
 ./build-rr.sh -d $DESTDIR -o ./obj $PLATFORM install
+
+fi
