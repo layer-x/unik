b8354ca7b94d1a05f2272539b423544062b77e18
Scott Weiss <scott.weiss@emc.com>
2016-03-09 17:49:36 -0500

    add osvcompiler to Makefile, add go wrapper stuff to yuval's version of the gomaincaller; broadcast mdns in daemon main
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -6,6 +6,8 @@ import (
 	"os/exec"
 	"flag"
 "github.com/layer-x/unik/pkg/daemon"
+	"os"
+	"github.com/hashicorp/mdns"
 )
 
 func main() {
@@ -15,23 +17,42 @@ func main() {
 	if *debugMode == "true" {
 		lxlog.ActiveDebugMode()
 	}
-	buildCommand := exec.Command("docker", "build", "-t", "rumpstager", ".")
-	buildCommand.Dir = "../../rumpstager"
+
+	buildCommand := exec.Command("make")
+	buildCommand.Dir = "../../containers/"
 	lxlog.LogCommand(buildCommand, true)
 	err := buildCommand.Run()
 	if err != nil {
-		lxlog.Errorf(logrus.Fields{"err": err}, "building rumpstager")
-		return
+		lxlog.Errorf(logrus.Fields{"err": err}, "building containers")
+		os.Exit(-1);
 	}
-	buildCommand = exec.Command("docker", "build", "-t", "rumpstager", ".")
-	buildCommand.Dir = "../../rumpcompiler"
-	lxlog.LogCommand(buildCommand, true)
-	err = buildCommand.Run()
+
+	lxlog.Infof(logrus.Fields{}, "all images finished")
+
+	host, err := os.Hostname()
 	if err != nil {
-		lxlog.Errorf(logrus.Fields{"err": err}, "building rumpstager")
-		return
+		lxlog.Errorf(logrus.Fields{"err": err}, "retreiving hostname")
+		os.Exit(-1);
 	}
-	lxlog.Infof(logrus.Fields{}, "built rumpstager image")
+
+	//make sure we don't attempt to multicast on a public cloud
+	if *provider != "ec2" {
+		lxlog.Infof(logrus.Fields{"host": host},"Starting unik discovery service")
+		info := []string{"Unik"}
+		service, err := mdns.NewMDNSService(host, "_unik._tcp", "", "", 8000, nil, info)
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"err": err}, "creating new mDNS service")
+			os.Exit(-1);
+		}
+		server, err := mdns.NewServer(&mdns.Config{Zone: service})
+		if err != nil {
+			lxlog.Errorf(logrus.Fields{"err": err}, "starting mDNS server")
+			os.Exit(-1);
+		}
+		defer server.Shutdown()
+		lxlog.Infof(logrus.Fields{"server": server},"Started unik discovery service")
+	}
+
 	unikDaemon := daemon.NewUnikDaemon(*provider)
 	unikDaemon.Start(3000)
 }
--- containers/rumpcompiler/go/gomaincaller.go
+++ containers/rumpcompiler/go/gomaincaller.go
@@ -1,8 +1,59 @@
 package main
 
-import "C"
+import (
+	"C"
+	"os"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"github.com/hashicorp/mdns"
+)
 
 //export gomaincaller
 func gomaincaller() {
+	// Make a channel for results and start listening
+	ipChan := make(chan string)
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	go func() {
+		for entry := range entriesCh {
+			ipChan <- entry.AddrV4.String()
+		}
+	}()
+	var unikEndpoint string
+	// Start the lookup
+	err := mdns.Lookup("_unik._tcp", entriesCh)
+	if err != nil {
+		//assume we are running on aws
+		unikEndpoint = "http://169.254.169.254/latest/user-data"
+	} else {
+		unikEndpoint = "http://"+<- ipChan+"/bootstrap"
+	}
+
+	var instanceData UnikInstanceData
+
+	resp, err := http.Get(unikEndpoint)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(data, &instanceData)
+	if err != nil {
+		panic(err)
+	}
+	for key, value := range instanceData.Env {
+		os.Setenv(key, value)
+	}
+
 	main()
+}
+
+//make sure this remains the same as defined in
+//github.com/layer-x/unik/pkg/daemon/ec2api/run_unik_instance.go
+type UnikInstanceData struct {
+	Tags map[string]string `json:"Tags"`
+	Env  map[string]string `json:"Env"`
 }
