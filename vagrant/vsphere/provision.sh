6bb3241dffb03212c1678c6b9708cd92a5365417
Scott Weiss <scott.weiss@emc.com>
2016-03-10 08:26:34 -0500

    boostrap vsphere appliance
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ vagrant/vsphere/provision.sh
@@ -0,0 +1,2 @@
+ssh unik@unikip "sudo sudo apt-get update -y  && sudo apt-get install libxen-dev curl git build-essential rsync"
+rsync --verbose --archive --delete -z --copy-links --no-owner --no-group --rsync-path sudo rsync -e ssh -p 22 -o StrictHostKeyChecking=no -o IdentitiesOnly=true -o UserKnownHostsFile=/dev/null -i '/Users/pivotal/workspace/go/src/github.com/layer-x/LayerX-Vagrant/vagrantmesos-ncalifornia.pem' --exclude .vagrant/ /Users/pivotal/workspace/go/src/github.com/layer-x/ ubuntu@ec2-54-67-96-194.us-west-1.compute.amazonaws.com:/vagrant
