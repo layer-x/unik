d9b2533c411cf94307a57041d65da933a4a6fe10
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-03-22 21:04:58 +0200

    pi2pi jtag seems to be working

--- containers/jtagpi/board/raspberrypi-raspberrypi.tcl
+++ containers/jtagpi/board/raspberrypi-raspberrypi.tcl
@@ -2,4 +2,4 @@ source [find interface/sysfsgpio-raspberrypi.cfg]
 set _CHIPNAME sam3
 
 source [find target/raspberry.cfg]
-$_TARGETNAME configure -event gdb-attach { reset init }
+# $_TARGETNAME configure -event gdb-attach { reset init }
