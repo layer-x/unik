17240f475ae6b6e4a04d075741064f6763207ad2
Scott Weiss <scott.weiss@emc.com>
2016-04-01 16:44:35 -0400

    split vsphere unikernel build into java/golang and determine type dynamically
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/daemon/vsphere/vsphere_api/build_golang_unikernel.go
+++ pkg/daemon/vsphere/vsphere_api/build_golang_unikernel.go
@@ -1,92 +1,18 @@
 package vsphere_api
 import (
-	"mime/multipart"
 	"github.com/Sirupsen/logrus"
 	"github.com/layer-x/layerx-commons/lxlog"
 	"github.com/layer-x/layerx-commons/lxerrors"
 	"github.com/layer-x/unik/pkg/types"
 	"time"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"github.com/layer-x/layerx-commons/lxfileutils"
-	"io"
 	"os/exec"
 	"github.com/layer-x/unik/pkg/daemon/state"
 	"github.com/layer-x/unik/pkg/daemon/vsphere/vsphere_utils"
 )
 
-func BuildGoUnikernel(unikState *state.UnikState, creds Creds, unikernelName, force string, uploadedTar multipart.File, handler *multipart.FileHeader) error {
-	vsphereClient, err := vsphere_utils.NewVsphereClient(creds.URL)
-	if err != nil {
-		return lxerrors.New("initiating vsphere client connection", err)
-	}
+func BuildGolangUnikernel(unikState *state.UnikState, unikernelName, unikernelId, unikernelCompilationDir, vmdkFolder string, vsphereClient *vsphere_utils.VsphereClient) error {
 
-	unikernelId := unikernelName //vsphere specific
-	vmdkFolder := "unik/"+unikernelId
-
-	defer func() {
-		if err != nil {
-			lxlog.Errorf(logrus.Fields{"error": err}, "error encountered, cleaning up unikernel artifacts")
-			if !strings.Contains(err.Error(), "already exists") {
-				vsphereClient.Rmdir(vmdkFolder)
-				delete(unikState.Unikernels, unikernelId)
-			}
-		}
-	}()
-
-	unikernels, err := ListUnikernels(unikState)
-	if err != nil {
-		return lxerrors.New("could not retrieve list of unikernels", err)
-	}
-	for _, unikernel := range unikernels {
-		if unikernel.UnikernelName == unikernelName {
-			if strings.ToLower(force) == "true" {
-				lxlog.Warnf(logrus.Fields{"unikernelName": unikernelName, "ami": unikernel.Id},
-					"deleting unikernel before building new unikernel")
-				err = DeleteUnikernel(unikState, creds, unikernel.Id, true)
-				if err != nil {
-					return lxerrors.New("could not delete unikernel", err)
-				}
-			} else {
-				return lxerrors.New("a unikernel already exists for this unikernel. try again with force=true", err)
-			}
-		}
-	}
-
-	unikernelCompilationDir, err := ioutil.TempDir(os.TempDir(), unikernelName+"-src-dir")
-	if err != nil {
-		return lxerrors.New("creating temporary directory "+unikernelName+"-src-dir", err)
-	}
-	//clean up artifacts even if we fail
-	defer func() {
-		err = os.RemoveAll(unikernelCompilationDir)
-		if err != nil {
-			panic(lxerrors.New("cleaning up unikernel files", err))
-		}
-		lxlog.Infof(logrus.Fields{"files": unikernelCompilationDir}, "cleaned up files")
-	}()
-	lxlog.Infof(logrus.Fields{"path": unikernelCompilationDir, "unikernel_name": unikernelName}, "created output directory for unikernel")
-	savedTar, err := os.OpenFile(unikernelCompilationDir +"/" + filepath.Base(handler.Filename), os.O_CREATE | os.O_RDWR, 0666)
-	if err != nil {
-		return lxerrors.New("creating empty file for copying to", err)
-	}
-	defer savedTar.Close()
-	bytesWritten, err := io.Copy(savedTar, uploadedTar)
-	if err != nil {
-		return lxerrors.New("copying uploaded file to disk", err)
-	}
-	lxlog.Infof(logrus.Fields{"bytes": bytesWritten}, "file written to disk")
-	err = lxfileutils.Untar(savedTar.Name(), unikernelCompilationDir)
-	if err != nil {
-		lxlog.Warnf(logrus.Fields{"saved tar name":savedTar.Name()}, "failed to untar using gzip, trying again without")
-		err = lxfileutils.UntarNogzip(savedTar.Name(), unikernelCompilationDir)
-		if err != nil {
-			return lxerrors.New("untarring saved tar", err)
-		}
-	}
-	lxlog.Infof(logrus.Fields{"path": unikernelCompilationDir, "unikernel_name": unikernelName}, "unikernel tarball untarred")
+	lxlog.Infof(logrus.Fields{"path": unikernelCompilationDir, "unikernel_name": unikernelName}, "building golang unikernel")
 
 	buildUnikernelCommand := exec.Command("docker", "run",
 		"--rm",
@@ -96,7 +22,7 @@ func BuildGoUnikernel(unikState *state.UnikState, creds Creds, unikernelName, fo
 	)
 	lxlog.Infof(logrus.Fields{"cmd": buildUnikernelCommand.Args}, "running build kernel command")
 	lxlog.LogCommand(buildUnikernelCommand, true)
-	err = buildUnikernelCommand.Run()
+	err := buildUnikernelCommand.Run()
 	if err != nil {
 		return lxerrors.New("building unikernel kernel failed", err)
 	}
